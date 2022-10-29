3c9314ca6bd72449cb536ed00399d1875bb7453e
jpereyra00 <jorgeagustinpereyra@gmail.com>
2022-10-28 21:31:26 -0300

    images js

--- images.js
+++ images.js
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head><title>Principal</title></head>
-    <body><h3>Bienvenidos a mi sitio web</h3></body>
+    <head><title>Home</title></head>
+    <body><h3>Bienvenidos a la terminal</h3></body>
 
 </html>
